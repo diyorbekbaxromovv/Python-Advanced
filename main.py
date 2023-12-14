7e9ab216c3d8657ebae8ec13e0a2b2dc84f613f6
Diyorbek <desktor00@gmail.com>
2023-12-14 17:48:14 +0500

    first code0

---
+++ main.py
@@ -0,0 +1,26 @@
+# import decimal
+
+# from decimal import Decimal, getcontext, Context
+
+# # import math
+
+# # print(math.sqrt(0.4))  math python 
+# print(Decimal(0.4).sqrt())  # bu python Decimal built-in function
+#######################################
+# getcontext().traps[decimal.DivisionByZero] = False
+
+# print(Decimal('Infinity'))
+#######################################
+
+# a = Decimal(0.3)
+
+# print(a.quantize(Decimal("0.000")))
+# print(a)
+#######################################
+
+
+text = "Hello my name is George"
+
+
+if type(text) is str:
+    print(text)
